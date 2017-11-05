ef5eec5305ae3a1559ec6f5e0ca7e96e71126e4f
Bruno Gonzaga <itsshowtime4@hotmail.de>
2017-11-05 15:51:06 +0100

    first commit

---
+++ Frontend/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { QuoteComponent } from './quote/quote.component';
+import { QuotesComponent } from './quotes/quotes.component';
+import { NewQuoteComponent } from './new-quote/new-quote.component';
+import { routing } from './app.routing';
+import { QuoteService } from './quote.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    QuoteComponent,
+    QuotesComponent,
+    NewQuoteComponent
+  ],
+  imports: [
+    BrowserModule,
+    routing,
+    HttpModule,
+  ],
+  providers: [QuoteService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
